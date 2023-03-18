b8c9d5593f4db9c184dd2070ce80b58a25f8aa38
Ha Phi Hung <haphihung55@gmail.com>
2023-03-19 01:12:01 +0700

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
+
+const apiClient = new ApolloClient({
+  uri: 'localhost:3000/',
+  cache: new InMemoryCache(),
+});
+
+function App(): JSX.Element {
+  return (
+    <ApolloProvider client={apiClient}>
+      <NavigationContainer>
+        <SafeAreaView>
+          <Text style={styles.titleText}>Hello World</Text>
+        </SafeAreaView>
+      </NavigationContainer>
+    </ApolloProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleText: {
+    fontSize: 25,
+  },
+});
+
+export default App;
