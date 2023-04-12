b40b5d3be53eee16a0ab01656e4d5d6c9f0baefc
Ha Phi Hung <haphihung55@gmail.com>
2023-04-12 21:59:14 +0700

    Remove apollo client code

--- App.tsx
+++ App.tsx
@@ -8,23 +8,15 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
 import {Typography, Colors} from './src/styles';
-
-const apiClient = new ApolloClient({
-  uri: 'localhost:3000/',
-  cache: new InMemoryCache(),
-});
 
 function App(): JSX.Element {
   return (
-    <ApolloProvider client={apiClient}>
-      <NavigationContainer>
-        <SafeAreaView style={styles.appBackground}>
-          <Text style={styles.titleText}>Hello World</Text>
-        </SafeAreaView>
-      </NavigationContainer>
-    </ApolloProvider>
+    <NavigationContainer>
+      <SafeAreaView style={styles.appBackground}>
+        <Text style={styles.titleText}>Hello World</Text>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
 
