960318716588080f493b3d03d50cdc69e9e106b1
nhutdanh175 <nhutdanh175@gmail.com>
2023-04-29 21:24:33 +0700

    set up styles

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -25,3 +25,11 @@ export const success = {
 export const warning = {
   main: '#',
 };
+
+export const appColor = {
+  primary: '#EF3651',
+  white: '#F6F6F6',
+  dark_black: '#1E1F28',
+  medium_black: '#2A2C36',
+  light_grey: '#ABB4BD',
+};
--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -14,10 +14,23 @@ export const heading = {
     fontSize: 16,
   },
   title: {
-    color: Colors.neutral.white,
+    color: Colors.appColor.primary,
     fontSize: 24,
   },
   cardTitle: {
     fontSize: 16,
   },
+};
+
+export const appFontSize = {
+  size_8: 8, // for note details,...
+  size_10: 10, // for item time text
+  size_12: 12, // for item text
+  size_16: 16, // for title item text
+  size_24: 24, // for title text
+  size_36: 36, // for large title text
+};
+
+export const appFont = {
+  primaryFont: 'Poppins',
 };
