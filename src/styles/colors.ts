3c828f49510bf9d3b6f66e6aab6e0c76ead1215f
nhutdanh175 <nhutdanh175@gmail.com>
2023-05-04 01:08:02 +0700

    delete redundant base style

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,31 +1,3 @@
-export const neutral = {
-  white: '#FFFFFF',
-};
-
-export const primary = {
-  brand: '#13678A',
-};
-
-export const secondary = {
-  accent: '#EF3651',
-};
-
-export const background = {
-  app: '#1E1F28',
-};
-
-export const danger = {
-  main: '#FF3E3E',
-};
-
-export const success = {
-  main: '#55D85A',
-};
-
-export const warning = {
-  main: '#',
-};
-
 export const appColor = {
   primary: '#EF3651',
   white: '#F6F6F6',
--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -1,27 +1,3 @@
-import * as Colors from './colors';
-
-export const body = {
-  main: {
-    fontSize: 16,
-  },
-};
-
-export const heading = {
-  section: {
-    fontSize: 16,
-  },
-  subtitle: {
-    fontSize: 16,
-  },
-  title: {
-    color: Colors.appColor.primary,
-    fontSize: 24,
-  },
-  cardTitle: {
-    fontSize: 16,
-  },
-};
-
 export const appFontSize = {
   size_8: 8, // for note details,...
   size_10: 10, // for item time text
