271b10bc0d75f55772f4a6ee7201b9309386831f
nhutdanh175 <nhutdanh175@gmail.com>
2023-05-08 01:44:15 +0700

    clean test in homeScreen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -7,60 +7,10 @@ import {UserWatchedListScreen} from './UserWatchedList';
 import {UserReviewsList} from './UserReviewsList';
 import {UserDetailsScreen} from './UserDetails';
 import {UserLikedCommentsList} from './UserLikedCommentsList';
-import {StyleSheet} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
-import {Text} from 'react-native';
-import {fontSizes, fonts} from '../styles/typography';
-
-import {CheckBox} from '@rneui/themed';
-import {Button} from '@rneui/themed';
-import {Input} from '@rneui/themed';
-
-import Icon from 'react-native-vector-icons/FontAwesome';
 
 export function HomeScreen(): JSX.Element {
-  const [checked, setChecked] = React.useState(true);
-  const toggleCheckbox = () => setChecked(!checked);
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>A quick brown fox jumps over the lazy dog</Text>
-      <Icon name="rocket" size={30} color="#900" />
-      <Button>Primary</Button>
-      <Button color="secondary">Secondary</Button>
-      <Button color="warning">Warning</Button>
-      <Button color="error">Error</Button>
-      <Input
-        placeholder="INPUT WITH ICON"
-        leftIcon={{type: 'font-awesome', name: 'chevron-left'}}
-      />
-      <CheckBox
-        checked={checked}
-        onPress={toggleCheckbox}
-        // Use ThemeProvider to make change for all checkbox
-        iconType="material-community"
-        checkedIcon="checkbox-marked"
-        uncheckedIcon="checkbox-blank-outline"
-        checkedColor="red"
-      />
-    </SafeAreaView>
-  );
+  return <></>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    height: '100%',
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  text: {
-    fontFamily: fonts.primary,
-    fontSize: fontSizes.xl,
-  },
-});
 
 export function HomeStackScreen(): JSX.Element {
   return (
