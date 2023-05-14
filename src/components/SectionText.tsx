d0dc6d3a4166b31d5647bc8a1db342c70547a53c
nhutdanh175 <nhutdanh175@gmail.com>
2023-05-14 15:21:23 +0700

    change text props

--- src/components/SectionText.tsx
+++ src/components/SectionText.tsx
@@ -4,14 +4,8 @@ import {fontSizes, fonts} from '../styles/typography';
 import colors from '../styles/colors';
 import {TextProps} from '../props/TextProps';
 
-/**
- *  props:
- *    text: the text want to display
- *  using: <SectionText text='aaaa'/>
- *
- */
 export function SectionText(props: TextProps): JSX.Element {
-  return <Text style={styles.text}>{props.text}</Text>;
+  return <Text style={styles.text}>{props.children}</Text>;
 }
 
 const styles = StyleSheet.create({
--- src/props/TextProps.ts
+++ src/props/TextProps.ts
@@ -1,3 +1,3 @@
 export interface TextProps {
-  text: string;
+  children: React.ReactNode;
 }
