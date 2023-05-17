e17ebcf4464908793964ebdc996a8ef8f38d716e
Ha Phi Hung <haphihung55@gmail.com>
2023-05-18 00:11:01 +0700

    Split dialog buttons into their own components

---
+++ src/components/DialogButtons.tsx
@@ -0,0 +1,60 @@
+import {Dialog} from '@rneui/themed';
+import {StyleSheet} from 'react-native';
+import colors from '../styles/colors';
+import {fontSizes, fonts} from '../styles/typography';
+import {ActionCb} from '../types/ActionCb';
+
+export interface DialogButtonProps {
+  onPress: ActionCb;
+}
+
+export function YesDialogButton(props: DialogButtonProps) {
+  return (
+    <Dialog.Button
+      title="YES"
+      buttonStyle={styles.yesDialogButton}
+      titleStyle={styles.subTextDialog}
+      onPress={props.onPress}
+    />
+  );
+}
+
+export function NoDialogButton(props: DialogButtonProps) {
+  return (
+    <Dialog.Button
+      title="NO"
+      buttonStyle={styles.noDialogButton}
+      titleStyle={styles.subTextDialog}
+      onPress={props.onPress}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  yesDialogButton: {
+    borderRadius: 12,
+    backgroundColor: colors.primary,
+    paddingVertical: 4,
+    paddingHorizontal: 32,
+  },
+  noDialogButton: {
+    borderRadius: 12,
+    backgroundColor: 'none',
+    borderWidth: 0.5,
+    borderColor: colors.white,
+    paddingVertical: 4,
+    paddingHorizontal: 32,
+  },
+  titleTextDialog: {
+    color: colors.white,
+    fontFamily: fonts.primary_bold,
+    fontSize: fontSizes.lg,
+    textAlign: 'left',
+  },
+  subTextDialog: {
+    color: colors.white,
+    fontFamily: fonts.primary,
+    fontSize: fontSizes.md,
+    textAlign: 'left',
+  },
+});
