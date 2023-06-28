eac476c651ef7fa18d9fb89fc7050121654f99f8
Ha Phi Hung <haphihung55@gmail.com>
2023-06-29 03:52:23 +0700

    Setup eslint for relay

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,6 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', 'plugin:relay/recommended'],
+  plugins: ['relay'],
+  ignorePatterns: ['**/__generated__/**'],
 };
