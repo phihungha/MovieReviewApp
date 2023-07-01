2eb7be41b642bd8debd9a6c0af75a7b933d6936b
Ha Phi Hung <haphihung55@gmail.com>
2023-07-01 21:44:44 +0700

    Disable faulty eslint relay rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,8 @@ module.exports = {
   root: true,
   extends: ['@react-native', 'plugin:relay/recommended'],
   plugins: ['relay'],
+  rules: {
+    'relay/generated-flow-types': 'off',
+  },
   ignorePatterns: ['**/__generated__/**'],
 };
