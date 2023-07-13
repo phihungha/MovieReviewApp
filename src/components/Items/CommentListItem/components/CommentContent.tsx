50d26918ae61280d50d6a75e21841b8023d864aa
Ha Phi Hung <haphihung55@gmail.com>
2023-07-13 19:14:41 +0700

    Fix warnings

--- src/components/Items/CommentListItem/components/CommentContent.tsx
+++ src/components/Items/CommentListItem/components/CommentContent.tsx
@@ -2,6 +2,7 @@ import {graphql} from 'relay-runtime';
 import {CommentContent$key} from './__generated__/CommentContent.graphql';
 import {useFragment} from 'react-relay';
 import {RegularText} from '../../../Text/RegularText';
+import React from 'react';
 
 const CommentContentFragment = graphql`
   fragment CommentContent on Comment {
@@ -13,7 +14,9 @@ export interface CommentContentProps {
   comment: CommentContent$key | null;
 }
 
-export function CommentContent({comment}: CommentContentProps) {
+export function CommentContent({
+  comment,
+}: CommentContentProps): React.JSX.Element {
   const data = useFragment(CommentContentFragment, comment);
   const content = data?.content ?? 'N/A';
   return <RegularText>{content === '' ? '[Deleted]' : content}</RegularText>;
